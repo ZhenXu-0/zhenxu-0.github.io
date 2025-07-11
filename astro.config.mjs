b17d57cb8758e9db504c81eae8162c237deb07ec
ZhenXu-0 <zx2393@tc.columbia.edu>
2025-07-11 00:31:12 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
+
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
+// IMPORTANT: Update the `site` and `base` settings
 export default defineConfig({
+  site: 'https://zhenxu-0.github.io/zhenxu.github.io',
   base: '/zhenxu.github.io/',
+  integrations: [mdx(), sitemap(), tailwind()]
 });
