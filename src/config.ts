217c86b284e66169f1aeb180a1e4ce7c8d81fcac
ZhenXu-0 <zx2393@tc.columbia.edu>
2025-07-17 06:54:47 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Zhen Xu, Teachers College, Columbia University';
+export const SITE_TITLE = 'Zhen Xu';
 export const SITE_DESCRIPTION = 'Zhen Xu is a PhD student in Cognitive Science at Teachers College, Columbia University. Explore research, projects, CV, and more.';
 export const GENERATE_SLUG_FROM_TITLE = true;
 export const TRANSITION_API = true;
